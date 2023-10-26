ae030d0a009e2cd137c3666bd3d32080c2234633
ZEDz318 <danzzeidan8884@gmail.com>
2023-10-26 23:46:31 +0200

    Done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # -explainable_recommender_systems
+
+This project was created using the [EMRBots](http://www.emrbots.org/) dataset to create an Explainable Recommender System capable recommending diagnosis to be looked out for, for patients based on their test results.
+
+## File Description
+
+Here you'll find a description of each file and the order of which to view them, as well as the order the system uses to serve its purpose
+
+1. **README.md:** Hopefully self-explanatory
+
+2. **Data:** contains the original txt files of the dataset and the csv version after conversion
+   * *txtData* : Folder containing the original txt file from the dataset
+   * *convert_txt_csv.py*: the file used to convert the txt data into csv
+   * *csvData*: Contains the datasets after conversion to csv:
+     * AdmissionsDiagnosesCorePopulatedTable.csv and LabsCorePopulatedTable.csv are the original txt files converted to csv
+     * AdmissionsDiagnosesCorePopulatedTableEdited.csv is the AdmissionsDiagnosesCorePopulatedTable.csv without time (which was deemed irrelevant) that was created in dataManipulation.py
+     * patient_for_recommendation.csv is the input, meaning it is the patient's data that we're using recommendations
+     * finalDataset.csv is the combination of the two datasets that is created in dataManipulation.py and used to calculate the cosine similarity
+
+3. **dataManipulation.py:** a python file that manipulates AdmissionsDiagnosesCorePopulatedTable.csv and creates AdmissionsDiagnosesCorePopulatedTableEdited.csv and finalDataset.csv
+4. **main.py:** This is where the magic happens. finalDataset.csv is used together with patient_for_recommendation.csv to calculate cosine similarity and recommend diagnosis to be tested
+5. **final_recommendation.csv:** Is where you'll find the final output
--- dataManipulation.py
+++ dataManipulation.py
@@ -1,7 +1,4 @@
-import os
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 
 # Load data
 diagnoses_data = pd.read_csv('data/csvData/AdmissionsDiagnosesCorePopulatedTable.csv')
@@ -11,42 +8,22 @@ labs_data = pd.read_csv('data/csvData/LabsCorePopulatedTable.csv')
 diagnoses_data.fillna(0, inplace=True)
 labs_data.fillna(0, inplace=True)
 
-# Add a column called PrimaryDiagnosisSubCode to split PrimaryDiagnosisCode into main diagnosis and subType
-sub_array = []
-main_array = []
-for i in diagnoses_data['PrimaryDiagnosisCode']:
-    main_array.append(i.split(".")[0])
-    if len(i.split(".")) > 1:
-        sub_array.append(i.split(".")[1])
-    else:
-        sub_array.append('-')
-
-diagnoses_data['PrimaryDiagnosisCode'] = main_array
-diagnoses_data['PrimaryDiagnosisSubCode'] = sub_array
-
 diagnoses_data.to_csv('data/csvData/AdmissionsDiagnosesCorePopulatedTableEdited.csv', index=False)
 
 # Convert to dataframe
-
 d = pd.DataFrame(diagnoses_data)
-l = pd.DataFrame(labs_data)
+labs_data = pd.DataFrame(labs_data).drop('LabDateTime', axis=1)
 
-l = l.drop('LabDateTime', axis=1)
-
-final_df = l.copy()
-primaryDiagnosisCode = [0] * len(l['PatientID'])
-primaryDiagnosisSubCode = [0] * len(l['PatientID'])
-primaryDiagnosisDescription = [0] * len(l['PatientID'])
+final_df = labs_data.copy()
+primaryDiagnosisCode = [0] * len(labs_data['PatientID'])
+primaryDiagnosisDescription = [0] * len(labs_data['PatientID'])
 
 for i in range(len(d)):
-    for j in range(len(l)):
-        if d['PatientID'][i] == l['PatientID'][j] and d['AdmissionID'][i] == l['AdmissionID'][j]:
+    for j in range(len(labs_data)):
+        if d['PatientID'][i] == labs_data['PatientID'][j] and d['AdmissionID'][i] == labs_data['AdmissionID'][j]:
             primaryDiagnosisCode[j] = d['PrimaryDiagnosisCode'][i]
-            primaryDiagnosisSubCode[j] = d['PrimaryDiagnosisSubCode'][i]
             primaryDiagnosisDescription[j] = d['PrimaryDiagnosisDescription'][i]
 
 final_df['PrimaryDiagnosisDescription'] = primaryDiagnosisDescription.copy()
 final_df['PrimaryDiagnosisCode'] = primaryDiagnosisCode.copy()
-final_df['PrimaryDiagnosisSubCode'] = primaryDiagnosisSubCode.copy()
-
 final_df.to_csv('data/csvData/finalDataset.csv', index=False)
